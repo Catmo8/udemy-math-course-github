2b344a9b88c75ab18e988915c62b1646b3c8113e
Catmo8 <catmo8@cox.net>
2019-04-14 17:16:18 -0500

    Did lecture 12-13

---
+++ Mathematics for Computer Games Development using Unity/Assets/Lecture 11/HitRay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitRay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        int layerMask = (1 << 10) | (1 << 9);
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),
+                out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+            Debug.Log("Missed");
+        }
+    }
+}
