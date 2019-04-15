77dee3c7216e5106049611345cb18f9f670ca519
Catmo8 <catmo8@cox.net>
2019-04-15 02:31:12 -0500

    Completed Lecture 17-18
    Still need to do challenge at the end of 18 before watching lecture 19 video.

---
+++ Mathematics for Computer Games Development using Unity/Assets/Section 3/Lecture 17-19/Coords.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coords
+{
+    float x;
+    float y;
+    float z;
+
+    public Coords(float _X, float _Y)
+    {
+        x = _X;
+        y = _Y;
+        z = -1;
+    }
+
+    public override string ToString()
+    {
+        return "(" + x + "," + y + "," + z + ")";
+    }
+    
+    static public void DrawPoint(Coords position, float width, Color colour)
+    {
+        GameObject line = new GameObject("Point_" + position.ToString());
+        LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+        lineRenderer.material.color = colour;
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, new Vector3(position.x - width / 3.0f, position.y - width / 3.0f, position.z));
+        lineRenderer.SetPosition(1, new Vector3(position.x + width / 3.0f, position.y + width / 3.0f, position.z));
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+    }
+    
+    /*Work on this code right here!!!
+    static public void DrawLine(Coords startPoint, Coords endPoint, float width, Color colour)
+    {
+        GameObject line = new GameObject("Point_" + position.ToString());
+        LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+        lineRenderer.material.color = colour;
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, new Vector3(position.x - width / 3.0f, position.y - width / 3.0f, position.z));
+        lineRenderer.SetPosition(1, new Vector3(position.x + width / 3.0f, position.y + width / 3.0f, position.z));
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+    }
+    */
+}
